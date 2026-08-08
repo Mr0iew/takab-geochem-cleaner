7c285a363c5a3ebe379d81a9d565b1cb7e4d761c
Ali <mr0iewz@gmail.com>
2026-08-08 17:31:16 +0330

    feat: add function to remove columns with >90% missing values

--- CleanData.py
+++ CleanData.py
@@ -4,6 +4,13 @@ import pandas as pd
 import numpy as np 
 
 def LoadData():  #load excel database
-    df = pd.read_excel("takab-data.xls", sheet_name="Sheet1")
+    df = pd.read_excel("TakabDatabase.xls", sheet_name="Sheet1")
     return df
 
+def RemoveEmptyColumns(df): #remove columns thiw > 90% missing values
+    threshold = len(df) * 0.9
+    df_clean = df.dropna(axis=1, thresh=threshold)
+    return df_clean
+
+df_raw = LoadData()
+df_clean = RemoveEmptyColumns(df_raw)
